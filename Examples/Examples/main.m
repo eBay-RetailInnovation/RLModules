f771a6d0d2e9b80c86ed7a28284dbaabb371e4e1
Nate Stedman <natesm@gmail.com>
2014-04-18 15:26:18 -0400

    Add examples project.

---
+++ Examples/Examples/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Examples
+//
+//  Created by Nate Stedman on 4/18/14.
+//  Copyright (c) 2014 eBay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EXAppDelegate class]));
+    }
+}
---
+++ Examples/ExamplesTests/ExamplesTests.m
@@ -0,0 +1,34 @@
+//
+//  ExamplesTests.m
+//  ExamplesTests
+//
+//  Created by Nate Stedman on 4/18/14.
+//  Copyright (c) 2014 eBay. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ExamplesTests : XCTestCase
+
+@end
+
+@implementation ExamplesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
