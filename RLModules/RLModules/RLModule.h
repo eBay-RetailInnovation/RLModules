ab11c65fcb1860a846d03a916284460046d28e92
Nate Stedman <natesm@gmail.com>
2014-04-18 11:12:29 -0400

    Add RLModule

---
+++ RLModules/RLModules/RLModule.h
@@ -0,0 +1,13 @@
+//
+//  RLModule.h
+//  RLModules
+//
+//  Created by Nate Stedman on 4/18/14.
+//  Copyright (c) 2014 eBay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RLModule : NSObject
+
+@end
---
+++ RLModules/RLModules/RLModule.m
@@ -0,0 +1,13 @@
+//
+//  RLModule.m
+//  RLModules
+//
+//  Created by Nate Stedman on 4/18/14.
+//  Copyright (c) 2014 eBay. All rights reserved.
+//
+
+#import "RLModule.h"
+
+@implementation RLModule
+
+@end
